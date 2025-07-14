c49905252252668ee808a444275a631001375a31
Darine <darinejeon@gmail.com>
2025-07-14 19:08:14 +0100

    Added Rock , Paper ,Scissors game main program

---
+++ project.py
@@ -0,0 +1,40 @@
+import random
+computerGuess=userGuess="Paper"
+roundNumber=1
+numberOfTrials=1
+playAgain="Y"
+print(" Welcome to the Rock, Paper, Scissors game! ")
+print("You will play against the computer.")
+options=["rock", "paper","scissors"]
+while True :
+    print("Round :", roundNumber);
+    computerGuess=random.choice(options)
+    userGuess=input("Please enter your guess : Paper , Rock , or Scissors :")
+    userGuess=userGuess.lower()
+    if userGuess not in options :
+        print("Invalid input , please try again.")
+        continue
+    if computerGuess == "Rock" and userGuess == "Scissors" or computerGuess== "Scissors" and userGuess == "Paper" or computerGuess == "Paper" and userGuess == "Rock":
+        print("Oh no Computer Wins!" ,computerGuess ,"beats " , userGuess,"hhhh")
+        numberOfTrials += 1
+    elif computerGuess == userGuess:
+        print("ّIt is a draw , you both chose : ", userGuess)
+        numberOfTrials += 1
+    else:
+        print("Yeeeey Congratulations! You win! ", userGuess, "beats", computerGuess)
+    playAgain=input("Do you wanna play again Y/N ? ").upper()
+    while True:
+            if playAgain == "Y":
+                roundNumber += 1
+                break
+            elif playAgain == "N":
+                print("Thanks for playing! You played ",roundNumber,"Rounds with", numberOfTrials, "trials ")
+                exit()
+            else:
+              playAgain=input("Invalid input, please enter Y or N.").upper()
+         
+
+    
+    
+
+
