d00de4eafaae405b591a0039ecbeaedc8d070381
Darine <darinejeon@gmail.com>
2025-07-14 21:30:57 +0100

    Introduces new output file and leaderboard display. Prompts players for names in both single and dual player modes and fixes case sensitivity.

--- project.py
+++ project.py
@@ -4,14 +4,26 @@ emojis={
     "paper": "✋",
     "scissors": "✌"
 }
+
 numberOfTrials=roundNumber=1
 numberOfWins=numberOfLosses=numberOfDraws=0
 numberofplayers=1
 options=["rock", "paper","scissors"]
 print(" Welcome to the Rock, Paper, Scissors game! ")
+
 while True:
  numberofplayers=input("choose 1 for one player mode and 2 for two players mode:")
+ def show_leaderboard():
+    print("\nLeaderboard:")
+    try:
+        with open("game_results.txt", "r") as file:
+            for line in file:
+                print(line)
+    except FileNotFoundError:
+        print("file not found!")
  if numberofplayers=="1" :
+  print("Please enter your name:")
+  userName = input().strip()
   print(" You will play against the computer.")
   while True :
     print("Round ", roundNumber," :");
@@ -21,7 +33,7 @@ while True:
     if userGuess not in options :
         print("Invalid input , please try again.")
         continue
-    if computerGuess == "Rock" and userGuess == "Scissors" or computerGuess== "Scissors" and userGuess == "Paper" or computerGuess == "Paper" and userGuess == "Rock":
+    if computerGuess == "rock" and userGuess == "scissors" or computerGuess== "scissors" and userGuess == "paper" or computerGuess == "paper" and userGuess == "rock":
         print("Oh no Computer Wins!" ,computerGuess ,emojis[computerGuess] ,"beats " , userGuess ,emojis[userGuess],"hhhh")
         
         numberOfLosses += 1
@@ -41,12 +53,22 @@ while True:
             elif playAgain == "N":
                 print("Thanks for playing! You played ",roundNumber,"Rounds with", numberOfTrials, "trials  and your score is:")
                 print("Wins:", numberOfWins, "\nLosses:", numberOfLosses, "\nDraws:", numberOfDraws)
+                with open("game_results.txt", "a") as file:
+                    file.write(f"{userName} played {roundNumber} rounds with {numberOfTrials} trials. Wins: {numberOfWins}, Losses: {numberOfLosses}, Draws: {numberOfDraws}\n")
+                print("Your results have been saved to game_results.txt")
+                show_leaderboard()
                 exit()
             else:
               playAgain=input("Invalid input, please enter Y or N.").upper()
+  
  elif numberofplayers=="2":
     playerOneWins=playerTwoWins=playerOneLosses=playerTwoLosses=Draws=0
+    roundNumber=1
     print(" You will play against anothe player:")
+    print("Player-One, please enter your name:")
+    playerOneName = input().strip()
+    print("Player-Two, please enter your name:")
+    playerTwoName = input().strip()
     while True: 
      print("Round ", roundNumber," :");
      playerOneGuess=input("Player-One Please enter your guess : Paper , Rock , or Scissors :")
@@ -87,6 +109,11 @@ while True:
                 print("Thanks for playing! You played ",roundNumber,"Rounds and your score is:")
                 print("Player-One Wins:", playerOneWins, "\nPlayer-Two Wins:", playerTwoWins, "\nDraws:", Draws)
                 print("Player-One Losses:", playerOneLosses, "\nPlayer-Two Losses:", playerTwoLosses)
+                with open("game_results.txt","a") as file:
+                    file.write(f"{playerOneName} played {roundNumber} rounds  won: {playerOneWins}, lost: {playerOneLosses} with {playerTwoName} who won: {playerTwoWins}, lost: {playerTwoLosses}, and for draws: {Draws}\n ")
+                print("Your results have been saved to game_results.txt")
+                show_leaderboard()
+               
                 exit()
             else:
               playAgain=input("Invalid input, please enter Y or N.").upper()
